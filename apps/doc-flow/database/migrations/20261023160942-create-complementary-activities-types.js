81f39a9447eb836f28ec2e554ccf6bf312d7dc01
ecbDeveloper <developer.ecb@gmail.com>
2025-10-25 03:31:34 -0300

    ajusta erro de sintaxe migration

--- apps/doc-flow/database/migrations/20261023160942-create-complementary-activities-types.js
+++ apps/doc-flow/database/migrations/20261023160942-create-complementary-activities-types.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-odule.exports = {
+module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('complementary_activities_types', {
       id: {
